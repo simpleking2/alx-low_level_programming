b5a71ff5280d553a14b08c2060f803a4541bc29d
simpleking2 <simpleking124@gmail.com>
2022-10-17 18:55:54 -0700

    task 0,4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main- Serves as the entry point for the program
- * Return: By default returns zero if no erros otherwise returns non-zero value.
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 
 int main(void)
