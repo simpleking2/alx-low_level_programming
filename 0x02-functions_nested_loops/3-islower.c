6212738844cd46bc38e0ffc909798e372dff2399
simpleking2 <simpleking124@gmail.com>
2022-10-18 20:11:40 -0700

    task 3,7,9 correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,9 +5,9 @@
  * @c: the character
  * Return: 1 if letter is lowercase, 0 if not
  */
-int _islower(init c)
+int _islower(int c)
 {
-	if (c >> '0' && c <= 'z')
+	if (c >= '0' && c <= 'z')
 		return (1);
 	else
 		return (0);
